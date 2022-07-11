ae9d9c4f379419f8ad402cb2d311943c759a2ae1
David Siamon <davidsiamon@Davids-MacBook-Pro.local>
2022-07-11 11:48:07 -0400

    Updates

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import setup
 
 setup(name = "booklover",
      version = "1.0",
-     description = "Books are so cool I love books booooooooooks books books",
+     description = "Books are really cool I love books booooooooooks books 
+books",
      url = "https://github.com/davsiam57349/HW14",
      author = "David Siamon",
      author_email = "dws3qd@virginia.edu",
